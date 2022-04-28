8765e32d0e5deb8416d65adbd6869d9da972f023
komedy2205 <komedy2205@gmail.com>
2022-04-28 21:00:45 +0300

    final

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { data } from "./data";
 export default function App() { 
   return (
     <div className="container mt-5 text-center">
-      <h3>React Spinners</h3>
+      <h3>LOADING......</h3>
       <div className="row">
         {data.map((loader, index) => (
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2 p-5">
